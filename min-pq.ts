32cfea7cb79e9619569db39f4c8e8710e103e59c
krishp03 <78389562+krishp03@users.noreply.github.com>
2021-07-29 22:04:40 -0400

    Starting Min Priority Queue Implementation

---
+++ min-pq.ts
@@ -0,0 +1,22 @@
+class MinPQ<T> {
+
+    binaryHeap: T[];
+    size: number;
+
+    constructor(){
+        this.binaryHeap = []
+        this.size = 0;
+    }
+
+    getSize(): number {
+        return this.size;
+    }
+
+    insert() {
+        
+    }
+
+    rmMin() {
+    
+    }
+}
